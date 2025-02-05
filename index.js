013a8c2f03e75e38c3df01bcbe5f6afd3e54d45d
Tanishjaura <jauratanish0@gmail.com>
2025-02-05 15:17:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+require("dotenv").config()
+const express=require("express")
+const app=express()
+const cors=require("cors")
+const PORT=3000
+app.use(express.json())
+app.use(cors())
+
+app.post("/abc",async(req,res)=>{
+const data=await req.body
+res.json({
+    data:data
+})
+})
+
+app.listen(PORT,()=>{
+    console.log("server is working")
+})
